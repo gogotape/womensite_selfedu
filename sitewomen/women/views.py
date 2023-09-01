6357352df4170751de72dd5b3dae4fb607b1ac9d
gogotape <bobovmikhail@mail.ru>
2023-09-01 11:20:06 +0300

    add first urls and views for women app
    # lesson 4

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Страница приложения women")
+
+
+def categories(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Статьи по категориям</h1>")
