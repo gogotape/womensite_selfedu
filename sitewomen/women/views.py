5ea7977b15a3b2669d1c121d6ebe62c72a4e1aff
gogotape <bobovmikhail@mail.ru>
2023-09-06 16:47:26 +0300

    add redirect, reverse, path names
    # lesson 8

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.urls import reverse
+
 
 # Create your views here.
 
@@ -18,7 +20,8 @@ def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
 
 def archive(request: HttpRequest, year: int) -> HttpResponse:
     if year > 2023:
-        raise Http404
+        uri = reverse("cats", args=("sport", ))
+        return HttpResponsePermanentRedirect(uri)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
