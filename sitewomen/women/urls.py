b536e981af5d77bb22fa49f90413e9f1c5e49d51
gogotape <bobovmikhail@mail.ru>
2023-09-06 16:47:26 +0300

    add redirect, reverse, path names
    # lesson 8

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -7,8 +7,8 @@ register_converter(converters.FourDigitYearConverter, "year4")
 
 
 urlpatterns = [
-    path('', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
-    path(r'archive/<year4:year>/', views.archive),
+    path('', views.index, name='home'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path(r'archive/<year4:year>/', views.archive, name='archive'),
 ]
