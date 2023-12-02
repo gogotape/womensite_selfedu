9a1474f0035dd2909fc12e0f64b9015bb0fdb88f
Mikhail <bobovmikhail@mail.ru>
2023-12-02 23:03:51 +0300

    feat: add settings for displaying posts
    add attr for django.admin.ModelAdmin class: list_display, list_display_links, ordering, list_editable, list_per_page
    
    # lesson 38

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from .models import Women, Category
+
 
 # Register your models here.
-from .models import Women
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "time_create", "is_published", "cat")
+    list_display_links = ("title", "id")
+    ordering = ("time_create", "title")
+    list_editable = ("is_published",)
+    list_per_page = 5
 
-admin.site.register(Women)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
