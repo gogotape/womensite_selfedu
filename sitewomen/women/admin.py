222149ff19e9cd90bf1600c131e88fb350cc9d2b
gogotape <bobovmikhail@mail.ru>
2023-12-05 18:59:56 +0300

    refactor: add custom settings to WomenAdmin
    add method "save" to Women model; add some fields to WomenAdmin (fields, exclude, readonly_fields, prepopulated_fields, filter_horizontal)
    
    # lesson 41

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -23,6 +23,11 @@ class MarriedFilter(admin.SimpleListFilter):
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
+    # fields = ("title", "content", "slug", "cat", "husband")
+    exclude = ("is_published", )
+    # readonly_fields = ("slug", )
+    prepopulated_fields = {"slug": ("title", )}
+    filter_horizontal = ("tags", )
     list_display = ("title", "time_create", "is_published", "cat", "brief_info")
     list_display_links = ("title",)
     ordering = ("time_create", "title")
