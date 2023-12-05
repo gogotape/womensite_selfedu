71f625acc7bd41dd60ddef47589e6ff48ab0928d
gogotape <bobovmikhail@mail.ru>
2023-12-05 18:59:56 +0300

    refactor: add custom settings to WomenAdmin
    add method "save" to Women model; add some fields to WomenAdmin (fields, exclude, readonly_fields, prepopulated_fields, filter_horizontal)
    
    # lesson 41

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 
@@ -45,6 +46,10 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('show_post', kwargs={'post_slug': self.slug})
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
 
 class Category(models.Model):
