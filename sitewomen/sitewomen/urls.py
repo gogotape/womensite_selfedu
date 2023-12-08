c8d9d17bb3bff7bd4c124c5f784ffcf9404790af
gogotape <bobovmikhail@mail.ru>
2023-12-08 16:36:57 +0300

    feat: add photo displaying on html-doc and admin panel
    add photo displaying on index, post page and admin panel
    
    # lesson 50

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
+from sitewomen import settings
 from women.views import page_not_found
 
 urlpatterns = [
@@ -24,6 +26,9 @@ urlpatterns = [
     path('', include('women.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
 
