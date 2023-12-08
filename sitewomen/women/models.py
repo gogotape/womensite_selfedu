16d526ea29f8a0573d13ba176b42de57a028ce21
gogotape <bobovmikhail@mail.ru>
2023-12-08 15:30:40 +0300

    feat: add file loading (using form associated with db model)
    # lesson 49

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -27,6 +27,8 @@ class Women(models.Model):
                                 MinLengthValidator(5, message="Min 5 symbols"),
                                 MaxLengthValidator(100)],
                             )
+
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", default=None, blank=True, null=True, verbose_name="Фото")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
@@ -91,3 +93,7 @@ class Husband(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UploadFile(models.Model):
+    file = models.FileField(upload_to="upload_model")
