bb4390cfe4bfaa17bcc9a952dfead89c124a0a64
gogotape <bobovmikhail@mail.ru>
2023-12-11 15:11:17 +0300

    feat: add login/logout views and templates;
    using authenticate, login, logout, namespaces
    
    # lesson 60

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,12 +1,34 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import LoginUserForm
+
 
 # Create your views here.
 
 
 def login_user(request: HttpRequest):
-    return HttpResponse("login")
+    if request.method == "POST":
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'],
+                                )
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+    else:
+        form = LoginUserForm()
+    return render(request,
+                  'users/login.html',
+                  {'form': form}
+                  )
 
 
 def logout_user(request: HttpRequest):
-    return HttpResponse("logout")
+    logout(request)
+    return HttpResponseRedirect(reverse('users:login'))
