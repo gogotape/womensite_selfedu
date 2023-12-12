362aa23e70461b5c7e798c2ed314c6907a66394a
Mikhail <bobovmikhail@mail.ru>
2023-12-12 23:51:26 +0300

    feat: add context processor; change base html template;
    # lesson 61

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -2,7 +2,6 @@ menu = [
     {'title': 'О сайте', 'url_name': 'about'},
     {'title': 'Добавить статью', 'url_name': 'add_page'},
     {'title': 'Обратная связь', 'url_name': 'contacts'},
-    {'title': 'Войти', 'url_name': 'login'},
 ]
 
 
@@ -16,14 +15,10 @@ class DataMixin:
         if self.title_page:
             self.extra_context["title"] = self.title_page
 
-        if not self.extra_context.get("menu"):
-            self.extra_context["menu"] = menu
-
         if self.cat_selected is not None:
             self.extra_context["cat_selected"] = self.cat_selected
 
     def get_mixin_context(self, context: dict, **kwargs):
-        context["menu"] = menu
         context["cat_selected"] = None
         context.update(kwargs)
         return context
