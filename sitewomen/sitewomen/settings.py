508fa24bdc0ba77b5e05f6bed9990a4bd2b9f98d
Mikhail <bobovmikhail@mail.ru>
2023-12-12 23:51:26 +0300

    feat: add context processor; change base html template;
    # lesson 61

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'users.context_processors.get_women_context',
             ],
         },
     },
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = "media/"
