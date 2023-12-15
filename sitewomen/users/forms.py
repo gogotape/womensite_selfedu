e882f895d90e03978323103c87302d38784ffb27
Mikhail <bobovmikhail@mail.ru>
2023-12-15 23:56:01 +0300

    feat: add registration using func view and model form; create templates
    # lesson 64

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 
 
 class LoginUserForm(AuthenticationForm):
@@ -14,3 +15,31 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ["username", "password"]
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(label="Пароль")
+    repeated_password = forms.CharField(label="Повторите пароль")
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "email", "first_name", "last_name", "password", "repeated_password"]
+        labels = {
+            "email": "Почтовый ящик",
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+        }
+
+    def clean_repeated_password(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["repeated_password"]:
+            raise forms.ValidationError("Пароли не совпадают!")
+        return cd["password"]
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError("Этот email уже используется!")
+
+        return email
