a31e85926c606d0b6eece9aa20a01c8dde917420
Mikhail <bobovmikhail@mail.ru>
2023-12-23 17:38:03 +0300

    feat: add registration by email; add user profile page
    # lesson 66

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -46,3 +46,21 @@ class RegisterUserForm(UserCreationForm):
             raise forms.ValidationError("Этот email уже используется!")
 
         return email
+
+
+class UserProfileForm(forms.ModelForm):
+    username = forms.CharField(disabled=True, label="Логин", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(disabled=True, label="E-mail", widget=forms.TextInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "email", "first_name", "last_name"]
+        labels = {
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+        }
+
+        widgets = {
+            "first_name": forms.TextInput(attrs={'class': 'form-input'}),
+            "last_name": forms.TextInput(attrs={'class': 'form-input'}),
+        }
